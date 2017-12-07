b914ede0c183f2b29b440a1c2cf83714247206d5
Elevenet <32978866+Elevenet@users.noreply.github.com>
2017-12-08 00:10:34 +0300

    Update first.php

--- first.php
+++ first.php
@@ -26,20 +26,6 @@
                   fclose($handle);
             }     
 
-            if(isset($_POST['text2'])){
-                  $name1 = $_POST['text2'];
-                  $handle1 = fopen('names.txt','a');
-                  fwrite($handle1,$name1."\n");
-                  fclose($handle1);
-            }     
-
-            if(isset($_POST['text3'])){
-                  $name2 = $_POST['text3'];
-                  $handle2 = fopen('names.txt','a');
-                  fwrite($handle2,$name2."\n");
-                  fclose($handle2);
-            }     
-
             if(isset($_POST['email'])){
                   $name3 = $_POST['email'];
                   $handle3 = fopen('names.txt','a');
